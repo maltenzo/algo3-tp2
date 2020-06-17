dab73ac3c8b39f016388fbddd6aac7736819f7ae
enzo emanuel vigneti <evigneti@dc.uba.ar>
2020-06-17 11:55:06 -0300

    agregado insercion (incompleto)

---
+++ Insercion.cpp
@@ -0,0 +1,23 @@
+insercion(g, l){
+  res=[1, 2, 3];
+  libres =[4........n];
+  while(res.size() <= g.size()){
+    v = elegir(g, libres);
+    res.push_back(v);
+  }
+  return res;
+}
+
+elegir(g, libres){
+  res = 0;
+  if (MB){
+
+  }
+  else if (MC){
+
+  }
+  else if (ML){
+
+  }
+  return res;
+}
